231fe74aefcdbe965b3d698765c82ec09f11b3c7
Tom <tomas95go@gmail.com>
2025-06-22 23:31:10 -0300

    refactor: factory method to create positive numbers

--- src/tests/unit/value-objects.test.ts
+++ src/tests/unit/value-objects.test.ts
@@ -1,19 +1,23 @@
 class PositiveNumber {
-    constructor(readonly value: number) {
-        if(value < 0) {
+    private constructor(readonly value: number) {}
+
+    static create(value: number): PositiveNumber {
+        if (value < 0) {
             throw new Error('Negative numbers are not allowed');
         }
+
+        return new PositiveNumber(value);
     }
 }
 
 describe('Manage positive numbers', () => {
    it('Should create a positive number', () => {
-       const positiveNumber = new PositiveNumber(1);
+       const positiveNumber = PositiveNumber.create(1);
        expect(positiveNumber.value).toBe(1);
    });
 
    it('Should prevent the creation of a negative number', () => {
-       expect(() => new PositiveNumber(-1)).toThrow('Negative numbers are not allowed');
+       expect(() => PositiveNumber.create(-1)).toThrow('Negative numbers are not allowed');
    });
 
 });
