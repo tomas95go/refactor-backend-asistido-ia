6380b3440da208d04f276f50d8e9b8b91d6ff291
Tom <tomas95go@gmail.com>
2025-06-30 21:08:13 -0300

    green: should save a new order

---
+++ src/tests/integration/in-memory-repository.test.ts
@@ -0,0 +1,63 @@
+import {OrderRepository} from "../../order/domain/repository/repository";
+import {Id} from "../../order/domain/value-object/id";
+import {Order} from "../../order/domain/aggregate/order";
+import {OrderLine} from "../../order/domain/value-object/order-line";
+import {Address} from "../../order/domain/value-object/address";
+import {PositiveNumber} from "../../order/domain/value-object/positive-number";
+
+class InMemoryRepository implements OrderRepository  {
+
+    private readonly orders: Order[] = [];
+
+    static create(): InMemoryRepository {
+        return new InMemoryRepository();
+    }
+
+    async findById(id: Id): Promise<Order | null> {
+        throw Error('Method not implemented.');
+    };
+
+    async findAll(): Promise<Order[] | []> {
+        return this.orders;
+    };
+
+    async save(order: Order): Promise<void> {
+        this.orders.push(order);
+    };
+
+    async delete(id: Id): Promise<void> {
+        return Promise.resolve(undefined);
+    };
+
+}
+
+describe('Order repository (in memory) methods', () => {
+
+    let repository: InMemoryRepository;
+
+    beforeAll(() => {
+        repository = InMemoryRepository.create();
+    });
+
+    it('Should save a new order', async () => {
+        // Arrange
+        const shippingAddressPrimitives = 'Avenida siempreviva 101';
+        const orderItemsPrimitives = [{
+            productId: '32aba416-8455-4018-82c4-d56253c152e9',
+            quantity: 2,
+            price: 200
+        }];
+        const shippingAddress: Address = Address.create(shippingAddressPrimitives);
+        const orderItems: OrderLine[]= orderItemsPrimitives.map(item => OrderLine.create(Id.from(item.productId), PositiveNumber.create(item.quantity), PositiveNumber.create(item.price),));
+        const order: Order = Order.create(orderItems, shippingAddress);
+        // Act
+        await repository.save(order);
+        // Assert
+        const savedOrder: Order[] | [] = await repository.findAll();
+        expect(savedOrder[0].toDto().items[0].productId).toBe(orderItemsPrimitives[0].productId);
+        expect(savedOrder[0].toDto().items[0].quantity).toBe(orderItemsPrimitives[0].quantity);
+        expect(savedOrder[0].toDto().items[0].price).toBe(orderItemsPrimitives[0].price);
+        expect(savedOrder[0].toDto().shippingAddress).toBe(shippingAddressPrimitives);
+    });
+
+});
