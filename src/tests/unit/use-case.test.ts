28058766b232c3af6aac9b2ab6659345da2ce375
Tom <tomas95go@gmail.com>
2025-07-02 00:02:16 -0300

    green: should create a new order with discount

--- src/tests/unit/use-case.test.ts
+++ src/tests/unit/use-case.test.ts
@@ -1,6 +1,7 @@
 import {Factory} from "../../factory";
 import {OrderUseCase, RequestOrder} from "../../order/application/order";
 import {InMemoryRepository} from "../integration/in-memory-repository.test";
+import {DiscountCodes} from "../../order/domain/constant/discount-code";
 
 describe('Order use case', () => {
 
@@ -20,5 +21,22 @@ describe('Order use case', () => {
         const savedOrder = await orderUseCase.createOrderUseCase(newOrder);
         // Assert
         expect(savedOrder).toBe('Order created with total: 400');
+    });
+
+    it('Should create a new order with discount', async () => {
+        // Arrange
+        const newOrder: RequestOrder = {
+            items: [{
+                productId: '32aba416-8455-4018-82c4-d56253c152e9',
+                quantity: 2,
+                price: 200
+            }],
+            shippingAddress: 'Avenida siempreviva 101',
+            discountCode: DiscountCodes.DISCOUNT20
+        };
+        // Act
+        const savedOrder = await orderUseCase.createOrderUseCase(newOrder);
+        // Assert
+        expect(savedOrder).toBe('Order created with total: 320');
     });
 })
